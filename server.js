f08cc780aa226ce6c9220290effae9620cb6cbe0
Dell <ali.hassan.salman.2000@gmail.com>
2025-09-05 17:57:27 +0300

    finished

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-// install (ejs, morgan, init, mongoose)
+// install (ejs, morgan, init, mongoose) and npm i method-override morgan to delete from DB
 
 const dotenv = require("dotenv"); // require package
 dotenv.config(); // Loads the environment variables from .env file
 
 const express = require('express');
 const mongoose = require("mongoose"); // require package
+const methodOverride = require("method-override"); // method-override morgan
 const app = express();
 
 // Connect to MongoDB using the connection string in the .env file
@@ -13,14 +14,16 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-// Import the Fruit model
-const Fruit = require("./models/fruit.js");
 
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const Fruit = require("./models/fruit.js"); // Import the Fruit model
 
-// use the modle to convert to HTTP
-app.use(express.urlencoded({ extended: false }));
+const morgan = require('morgan'); // morgan to see all errors and dignosis
+app.use(morgan('dev')); // morgan to see all errors and dignosis
+
+app.use(express.urlencoded({ extended: false })); // use the modle to convert to HTTP
+
+app.use(methodOverride("_method")); // method-override morgan
+app.use(morgan("dev")); // method-override morgan
 
 /* ----------------------------------------------------------setup -------------------------------*/
 
@@ -32,6 +35,12 @@ app.get("/", async (req, res) => {
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
+});
+
+// GET /fruits/:id (this must be after new path)
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
 // POST /fruits
@@ -52,8 +61,44 @@ app.get("/fruits", async (req, res) => {
   res.render("fruits/index.ejs", { fruits: allFruits });
 });
 
+// delete function
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+// delete Item route
+app.delete("/fruits/:fruitId", (req, res) => {
+  res.send("This is the delete route");
+});
+
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  //console.log(foundFruit);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+// update function
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 /* ---------------------------------------------------------- Path */
+
 
 
 
